e1006be66c9b5c941a19017394277ac630207745
uwee <uweenukr@gmail.com>
2020-04-12 14:11:46 -0400

    Add objectManager to HostSetup for tests

--- Assets/Mirror/Tests/Runtime/HostSetup.cs
+++ Assets/Mirror/Tests/Runtime/HostSetup.cs
@@ -19,6 +19,7 @@ namespace Mirror.Tests
         protected NetworkManager manager;
         protected NetworkServer server;
         protected NetworkClient client;
+        protected ClientObjectManager clientObjectManager;
 
         protected GameObject playerGO;
         protected NetworkIdentity identity;
@@ -30,6 +31,7 @@ namespace Mirror.Tests
             networkManagerGo = new GameObject();
             networkManagerGo.AddComponent<MockTransport>();
             manager = networkManagerGo.AddComponent<NetworkManager>();
+            clientObjectManager = networkManagerGo.AddComponent<ClientObjectManager>();
             manager.client = networkManagerGo.GetComponent<NetworkClient>();
             manager.server = networkManagerGo.GetComponent<NetworkServer>();
             server = manager.server;
