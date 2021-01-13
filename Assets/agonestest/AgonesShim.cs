ecbcd64864d2f634bc9ad4d9d4360b94cd554dbd
uwee <uweenukr@gmail.com>
2021-01-13 15:34:52 -0500

    quick demo

---
+++ Assets/agonestest/AgonesShim.cs
@@ -0,0 +1,29 @@
+using Agones;
+using Mirror;
+using UnityEngine;
+
+public class AgonesShim : MonoBehaviour
+{
+    public NetworkServer server;
+    public AgonesSdk sdk;
+
+    public void Awake()
+    {
+        //server.Started.AddListener(ConnectWrapper);
+        server.Stopped.AddListener(ShutdownWrapper);
+    }
+
+    public async void ConnectWrapper()
+    {
+        if(await sdk.Connect())
+        {
+            _ = sdk.Ready();
+        }
+    }
+
+    public void ShutdownWrapper()
+    {
+        sdk.healthEnabled = false;
+        _ = sdk.Shutdown();
+    }
+}
