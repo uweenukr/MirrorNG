774af2db4fd39eacceed3ab7e39c607d3c4dc727
uwee <uweenukr@gmail.com>
2021-01-26 13:57:25 -0500

    add new empty solution

---
+++ MirrorStandalone/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MirrorStandalone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
