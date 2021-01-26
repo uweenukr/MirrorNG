89c5a8201bef44c56d6ad22bdb2e2ff7a34d5936
uwee <uweenukr@gmail.com>
2021-01-26 15:47:00 -0500

    remove object code from NS interface

--- MirrorStandalone/Runtime/INetworkServer.cs
+++ MirrorStandalone/Runtime/INetworkServer.cs
@@ -3,29 +3,6 @@ using UnityEngine;
 
 namespace Mirror
 {
-    public interface IServerObjectManager
-    {
-        bool AddPlayerForConnection(INetworkConnection conn, GameObject player);
-
-        bool AddPlayerForConnection(INetworkConnection conn, GameObject player, Guid assetId);
-
-        bool ReplacePlayerForConnection(INetworkConnection conn, NetworkClient client, GameObject player, bool keepAuthority = false);
-
-        bool ReplacePlayerForConnection(INetworkConnection conn, NetworkClient client, GameObject player, Guid assetId, bool keepAuthority = false);
-
-        void Spawn(GameObject obj, GameObject ownerPlayer);
-
-        void Spawn(GameObject obj, INetworkConnection ownerConnection = null);
-
-        void Spawn(GameObject obj, Guid assetId, INetworkConnection ownerConnection = null);
-
-        void Destroy(GameObject obj);
-
-        void UnSpawn(GameObject obj);
-
-        bool SpawnObjects();
-    }
-
     public interface INetworkServer
     {
         void Disconnect();
@@ -35,7 +12,5 @@ namespace Mirror
         void RemoveConnection(INetworkConnection conn);
 
         void SendToAll<T>(T msg, int channelId = Channel.Reliable);
-
-        void SendToClientOfPlayer<T>(NetworkIdentity identity, T msg, int channelId = Channel.Reliable);
     }
 }
