6d794031c29ee57933b088c7b6ba525ed1f2ac48
uwee <uweenukr@gmail.com>
2021-01-27 14:48:39 -0500

    refactor

--- MirrorStandalone/Program.cs
+++ MirrorStandalone/Program.cs
@@ -1,31 +1,14 @@
-﻿using Mirror.KCP;
-
-namespace Mirror.Standalone
+﻿namespace Mirror.Standalone
 {
-    public static class Globals
-    {
-        //I know...this is temporary!
-        public const string productName = "CHANGE_ME";
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            NetworkServer server = new NetworkServer();
-            KcpTransport servertransport =  new KcpTransport();
-            server.Transport = servertransport;
-            _ = server.ListenAsync();
-
-            NetworkClient client = new NetworkClient();
-            KcpTransport clienttransport = new KcpTransport();
-            client.Transport = clienttransport;
-            client.ConnectAsync("localhost");
+            StandaloneNG mirror = new StandaloneNG();
 
             while(true)
             {
-                servertransport.Update();
-                clienttransport.Update();
+                mirror.Update();
             }
         }
     }
---
+++ MirrorStandalone/StandaloneNG.cs
@@ -0,0 +1,38 @@
+﻿using Mirror.KCP;
+
+namespace Mirror.Standalone
+{
+    public static class Globals
+    {
+        //I know...this is temporary!
+        public const string productName = "CHANGE_ME";
+    }
+
+    public class StandaloneNG
+    {
+        public NetworkServer server;
+        KcpTransport servertransport;
+
+        public NetworkClient client;
+        KcpTransport clienttransport;
+
+        public StandaloneNG()
+        {
+            server = new NetworkServer();
+            servertransport = new KcpTransport();
+            server.Transport = servertransport;
+            _ = server.ListenAsync();
+
+            client = new NetworkClient();
+            clienttransport = new KcpTransport();
+            client.Transport = clienttransport;
+            client.ConnectAsync("localhost");
+        }
+
+        public void Update()
+        {
+            servertransport.Update();
+            clienttransport.Update();
+        }
+    }
+}
